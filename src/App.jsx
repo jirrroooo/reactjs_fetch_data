56253638a34e7c7df911a936393fa36528f5fb07
Jiro150801 <71065171+Jiro150801@users.noreply.github.com>
2023-07-11 16:26:46 +0800

    feat: fetched data from the API

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import Reddit from "./Reddit";
+import Joke from "./Joke";
+
+function App() {
+  const [redditVisible, setRedditVisible] = useState(false);
+  const [jokeVisible, setJokeVisible] = useState(false);
+
+  return (
+    <div>
+      <div className="buttons">
+        <button
+          onClick={() =>
+            setRedditVisible((prevRedditVisible) => !redditVisible)
+          }
+        >
+          Toggle Reddit
+        </button>
+        <button
+          onClick={() =>
+            setJokeVisible((prevJokeVisible) => !jokeVisible)
+          }
+        >
+          Toggle Joke
+        </button>
+      </div>
+      {redditVisible && <Reddit />}
+      {jokeVisible && <Joke />}
+    </div>
+  );
+}
+
+export default App;
